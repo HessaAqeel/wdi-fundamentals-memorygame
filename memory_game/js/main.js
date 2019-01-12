8cddd1f16cf3a495fcc6b444d5e6721af51ec643
hessa ali <hessa@hessas-MacBook-Pro.local>
2019-01-12 14:59:44 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -45,6 +45,7 @@ var cardsInPlay = [];
 
 var checkForMatch = function(){
 
+this.setAttribute(cards['queen'].images/queen-of-hearts.png)
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 console.log("You found a match!");
 } else {
@@ -52,8 +53,9 @@ console.log("Sorry, try again.");
 }
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
 
+	this.getAttribute('data-id');
 	console.log("User flipped" + cards[0].rank);
 	console.log("User flipped" + cards[3].rank);
 	cardsInPlay.push(cards[0].rank); 
@@ -67,12 +69,27 @@ checkForMatch();
     
 	
     flipCard(); 
-	flipCard(0);
-	flipCard(3);
+	
 
 
 
+var createBoard = function(){
+
+for (var i = 0; i < cards.length; i++) {
+
+var createElement = document.createElement('img');
+
+cardElement.setAttribute('img').src = "images/back.png";
+
+cardElement.setAttribute('data-id', i);
+
+addEventListener('click', flipCard);
+
+appendChild(cardElement, "game-board");
 
 
+}
+
+createBoard();
 
 
